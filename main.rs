4b062f8da7b8df6096bf0b1dc5b6b3433516ddfb
Vampire200500 <zane.mckenzie2005@gmail.com>
2022-11-27 15:26:54 +0000

    "Updated my stuff"

--- main.rs
+++ main.rs
@@ -1,11 +1,13 @@
 
 
-use std::{process::{Command}, result};
+use std::process::Command;
 fn main() {
 let result = Command::new("git").args(["pull"]).output();
 println!("{:?}", result);
 let result = Command::new("git").args(["add","."]).output();
 println!("{:?}", result);
 let result=Command::new("git").args(["commit","-m\"Updated my stuff\""]).output();
+println!("{:?}", result);
+let result=Command::new("git").args(["push"]).output();
 println!("{:?}", result);
 }
