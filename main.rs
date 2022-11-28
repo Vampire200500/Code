3cdd08398d69fa0b3cdc2b2fb03933b24349ff97
Vampire200500 <zane.mckenzie2005@gmail.com>
2022-11-28 11:37:17 +0000

    "Updated my stuff"

--- main.rs
+++ main.rs
@@ -9,7 +9,7 @@ let result=Command::new("git").args(["commit","-m\"Updated my stuff\""]).output(
 println!("{:?}", result);
 let result=Command::new("git").args(["push"]).output();
 println!("{:?}", result);
-let result=Command::new("start").args(["chrome.exe"]).output();
+let result=Command::new("start chrome").output();
 println!("{:?}", result);
 
 
