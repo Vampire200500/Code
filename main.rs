4baa7aed11655dd9c5107f89103f9c0d23e98bc5
Vampire200500 <zane.mckenzie2005@gmail.com>
2022-11-28 10:50:04 +0000

    "Updated my stuff"

--- main.rs
+++ main.rs
@@ -10,6 +10,6 @@ let result=Command::new("git").args(["commit","-m\"Updated my stuff\""]).output(
 println!("{:?}", result);
 let result=Command::new("git").args(["push"]).output();
 println!("{:?}", result);
-let cmd=Command::new("fsutil").args(["file", "create new","test.rs","0"]).output();
+let cmd=Command::new("fsutil").args(["file", "createNew","test.rs","0"]).output();
 println!("{:?}", cmd)
 }
