dad0deaab3bbd7ad4380a81d574245c996ec1aa7
Vampire200500 <zane.mckenzie2005@gmail.com>
2022-12-06 09:54:25 +0000

    "Updated my stuff"

--- main.rs
+++ main.rs
@@ -9,9 +9,6 @@ println!("{:?}", result);
 let result=Command::new("git").args(["commit","-m\"Updated my stuff\""]).output();
 println!("{:?}", result);
 let result=Command::new("git").args(["push"]).output();
-println!("{:?}", result);
-let result=Command::new("fsutil").args(["file","createnew","README.md","0"]).output();
-println!("{:?}", result);
-let result=Command::new("start").arg("chrome").output().expect("failed");
-println!("{:?}", result)
-}
+println!("{:?}", result);}
+//let result=Command::new("fsutil").args(["file","createnew","README.md","0"]).output();
+//println!("{:?}", result);}
